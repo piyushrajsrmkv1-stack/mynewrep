2979e9dc19e1e2b6dfe5bac5f894a072fba4902f
piyushrajsrmkv1-stack <piyushrajsrmkv1@gmail.com>
2026-05-17 20:01:09 +0530

    Add HelloWorld.java with basic Java program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to GitHub!");
+        System.out.println("This is my first Java program.");
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+
+        System.out.println("Addition Result: " + sum);
+    }
+}
