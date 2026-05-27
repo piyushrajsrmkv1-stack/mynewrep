7b16e879883b89e4ea39f3d96613af91595a8df3
piyushrajsrmkv1-stack <piyushrajsrmkv1@gmail.com>
2026-05-27 16:59:19 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+// app.js
+
+function calculateDiscount(price, discountPercentage) {
+
+    // ERROR 1: Missing closing parenthesis
+    if (discountPercentage > 50 {
+
+        console.log("High discount applied");
+    }
+
+    // ERROR 2: Undefined variable
+    let finalPrice = price - (price * discountRate / 100);
+
+    return finalPrice;
+}
+
+
+// ERROR 3: Infinite loop
+for (let i = 0; i < 5; i--) {
+
+    console.log(i);
+}
+
+
+// ERROR 4: Calling undefined function
+let result = getFinalAmount(1000, 20);
+
+console.log("Final Amount:", result);
+
+
+// ERROR 5: Unused variable
+let tempData = "AAVA_TEST";
+
+
+// ERROR 6: Formatting issue
+function  printMessage( ){
+console.log("Testing remediation tool")
+}
